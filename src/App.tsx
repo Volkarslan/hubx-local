1d61effc0b56c262379b81f759d730136893e204
Volkan Uğur ARSLAN <volkanugurarslan@gmail.com>
2024-01-17 13:54:36 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './App.module.css';
+
+export function App() {
+  return (
+    <>
+      <header>
+        <h1>HubX Frontend Assignment</h1>
+      </header>
+
+      <main className={styles.main}></main>
+    </>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './styles.css'
+import { App } from './App';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
